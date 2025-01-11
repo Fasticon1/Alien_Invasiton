11fddbe9b4fe50d16acdc587ec64d74cd7bd13e2
jeremy.hunt5@gmail.com <jeremy.hunt5@gmail.com>
2025-01-10 18:27:32 -0600

    created alien fleet, set left right and forward movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,7 +35,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self._update_bullets()            
+            self._update_bullets()
+            self._update_aliens()            
             self._update_screen()
             self.clock.tick(60)
 
@@ -85,20 +86,44 @@ class AlienInvasion:
     def _create_fleet(self):
         #Create the alien fleet.
         #Create an alien and keep adding aliens until there's no more room
-        # Spacing is one alien width apart.
+        # Spacing is one alien width and height apart.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self.create_alien(current_x)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 10 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
 
-    def create_alien(self, x_position):
+            #Finished a row; reset x vale, and increment y value.
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
             new_alien = Alien(self)
             new_alien.x = x_position 
             new_alien.rect.x = x_position
+            new_alien.rect.y = y_position
             self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        # Respond appropriately if any aliens have reached an edge.
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        # Drop the entire fleet and change direction.
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        # Check if fleet is at an edge, the update positions.
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
             # Redraw the screen durring each pass through the loop.
